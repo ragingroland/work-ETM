bfccb59eb863a6b5ec13642dccee0d07e1c7fc5a
Иванов Владимир <ragingroland@yandex.ru>
2024-09-18 12:17:33 +0300

    764 765

---
+++ vertica_scripts/BI-764+1/linux scripts/renew_ecom_adconv.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# переход к нужному каталогу
+cd /autons/vertica/web_analitik
+
+# проверка наличия флага завершения выгрузки
+while [ ! -f /autons/vertica/web_analitik/end_adcnv.flg ];
+    do
+        echo "Файл end_adcnv.flg не найден"
+        sleep 100
+done
+
+# проверка наличия флага уже запущенного процесса (именно вот этого вот всего)
+if [ -f /autons/vertica/web_analitik/start_adcnv.wrk ];
+    then
+        echo "Процесс уже запущен, выход."
+        exit 0
+    else
+        # так как флага .wrk обнаружено не было, начинается процесс загрузки в базу
+        echo "1_start: $(date)" > time_adcnv.log
+        echo "start: $(date)" > start_adcnv.wrk
+        echo "подключение к вертике"
+
+        PATH=/opt/vertica/bin:$PATH:.; export PATH
+        if [ -f /autons/vertica/web_analitik/ad_conversion_81.csv ];
+            then
+                vsql -U user_etl_adm -w useretladmvert92 -h 172.24.2.140 -p 5433 -d DWH -C -f /autons/vertica/web_analitik_run/vertica_scripts/renew_adconversion.vsql -A -q -o /autons/vertica/web_analitik_run/vertica_scripts/renew_adconversion.vout
+        fi
+fi
+
+rm start_adcnv.wrk
+rm end_adcnv.flg
+echo "2_stop: $(date)" >> time_adcnv.log
+exit 0
---
+++ vertica_scripts/BI-764+1/linux scripts/renew_ecom_spr.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# переход к нужному каталогу
+cd /autons/vertica/web_analitik
+
+# проверка наличия флага завершения выгрузки
+while [ ! -f /autons/vertica/web_analitik/end_spr.flg ];
+    do
+        echo "Файл end_spr.flg не найден"
+        sleep 100
+done
+
+echo "end_spr.flg присутствует"
+
+# проверка наличия флага уже запущенного процесса (именно вот этого вот всего)
+if [ -f /autons/vertica/web_analitik/start_spr.wrk ];
+    then
+        echo "Процесс уже запущен, выход."
+        exit 0
+    else
+        # так как флага .wrk обнаружено не было, начинается процесс загрузки в базу
+        echo "1_start: $(date)" > time_spr.log
+        echo "start: $(date)" > start_spr.wrk
+        echo "подключение к вертике"
+
+        PATH=/opt/vertica/bin:$PATH:.; export PATH
+        if [ -f /autons/vertica/web_analitik/Counter.csv ];
+            then
+                vsql -U user_etl_adm -w useretladmvert92 -h 172.24.2.140 -p 5433 -d DWH -C -f /autons/vertica/web_analitik_run/vertica_scripts/renew_counter.vsql -A -q -o /autons/vertica/web_analitik_run/vertica_scripts/renew_counter.vout
+        fi
+        if [ -f /autons/vertica/web_analitik/Countries.csv ];
+            then
+                vsql -U user_etl_adm -w useretladmvert92 -h 172.24.2.140 -p 5433 -d DWH -C -f /autons/vertica/web_analitik_run/vertica_scripts/renew_countries.vsql -A -q -o /autons/vertica/web_analitik_run/vertica_scripts/renew_countries.vout
+        fi
+        if [ -f /autons/vertica/web_analitik/Regions.csv ];
+            then
+                vsql -U user_etl_adm -w useretladmvert92 -h 172.24.2.140 -p 5433 -d DWH -C -f /autons/vertica/web_analitik_run/vertica_scripts/renew_regions.vsql -A -q -o /autons/vertica/web_analitik_run/vertica_scripts/renew_regions.vout
+        fi
+        if [ -f /autons/vertica/web_analitik/Cities.csv ];
+            then
+                vsql -U user_etl_adm -w useretladmvert92 -h 172.24.2.140 -p 5433 -d DWH -C -f /autons/vertica/web_analitik_run/vertica_scripts/renew_cities.vsql -A -q -o /autons/vertica/web_analitik_run/vertica_scripts/renew_cities.vout
+        fi
+        if [ -f /autons/vertica/web_analitik/TrafficSource.csv ];
+            then
+                vsql -U user_etl_adm -w useretladmvert92 -h 172.24.2.140 -p 5433 -d DWH -C -f /autons/vertica/web_analitik_run/vertica_scripts/renew_trafficsource.vsql -A -q -o /autons/vertica/web_analitik_run/vertica_scripts/renew_trafficsource.vout
+        fi
+        if [ -f /autons/vertica/web_analitik/SearchEngineRoot.csv ];
+            then
+                vsql -U user_etl_adm -w useretladmvert92 -h 172.24.2.140 -p 5433 -d DWH -C -f /autons/vertica/web_analitik_run/vertica_scripts/renew_searchengineroot.vsql -A -q -o /autons/vertica/web_analitik_run/vertica_scripts/renew_searchengineroot.vout
+        fi
+        if [ -f /autons/vertica/web_analitik/TrafficSourceFirstLast.csv ];
+            then
+                vsql -U user_etl_adm -w useretladmvert92 -h 172.24.2.140 -p 5433 -d DWH -C -f /autons/vertica/web_analitik_run/vertica_scripts/renew_trafficsourcefl.vsql -A -q -o /autons/vertica/web_analitik_run/vertica_scripts/renew_trafficsourcefl.vout
+        fi
+        if [ -f /autons/vertica/web_analitik/Status.csv ];
+            then
+                vsql -U user_etl_adm -w useretladmvert92 -h 172.24.2.140 -p 5433 -d DWH -C -f /autons/vertica/web_analitik_run/vertica_scripts/renew_status.vsql -A -q -o /autons/vertica/web_analitik_run/vertica_scripts/renew_status.vout
+        fi
+        if [ -f /autons/vertica/web_analitik/Campaing.csv ];
+            then
+                vsql -U user_etl_adm -w useretladmvert92 -h 172.24.2.140 -p 5433 -d DWH -C -f /autons/vertica/web_analitik_run/vertica_scripts/renew_campaing.vsql -A -q -o /autons/vertica/web_analitik_run/vertica_scripts/renew_campaing.vout
+        fi
+        if [ -f /autons/vertica/web_analitik/Category.csv ];
+            then
+                vsql -U user_etl_adm -w useretladmvert92 -h 172.24.2.140 -p 5433 -d DWH -C -f /autons/vertica/web_analitik_run/vertica_scripts/renew_category.vsql -A -q -o /autons/vertica/web_analitik_run/vertica_scripts/renew_category.vout
+        fi
+        if [ -f /autons/vertica/web_analitik/TypeClient.csv ];
+            then
+                vsql -U user_etl_adm -w useretladmvert92 -h 172.24.2.140 -p 5433 -d DWH -C -f /autons/vertica/web_analitik_run/vertica_scripts/renew_typeclient.vsql -A -q -o /autons/vertica/web_analitik_run/vertica_scripts/renew_typeclient.vout
+        fi
+        if [ -f /autons/vertica/web_analitik/AdType.csv ];
+            then
+                vsql -U user_etl_adm -w useretladmvert92 -h 172.24.2.140 -p 5433 -d DWH -C -f /autons/vertica/web_analitik_run/vertica_scripts/renew_adtype.vsql -A -q -o /autons/vertica/web_analitik_run/vertica_scripts/renew_adtype.vout
+        fi
+fi
+
+rm start_spr.wrk
+rm end_spr.flg
+echo "2_stop: $(date)" >> time_spr.log
+exit 0
